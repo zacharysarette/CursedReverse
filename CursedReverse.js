08f85195ec591c77ac6f3bb081e93281e9896789
Zachary W Sarette <zacharywsarette@gmail.com>
2022-04-09 22:40:21 +0900

    Even more anon funky business

--- CursedReverse.js
+++ CursedReverse.js
@@ -1,4 +1,4 @@
 #!/bin/env node
 /* Created By Zachary Sarette in April of 2022. Stay Fancy! */
-($ => {process.stdout.write($ ?? "Where Are The Args?")})(($ => $.join(""))(($ => $.reverse())(($=> $.split(""))(process.argv.slice(2).join(" ") ?? ""))))
+($ => {process.stdout.write($ ?? "Where Are The Args?")})(($ => $.join(""))(($ => $.reverse())(($=> $.split(""))(($=> $.join(" ") ?? "")(process.argv.slice(2))))))
 
